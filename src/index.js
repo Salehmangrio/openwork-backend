9d30c5f262f4e4c67593c745bdf84e4a5aee7083
Saleh Muhammad Mangrio <salehmangrio114@gmail.com>
2026-04-20 22:46:28 +0500

    update cors for test purpose

--- src/index.js
+++ src/index.js
@@ -20,9 +20,7 @@ app.set('trust proxy', 1);
 // ─── Socket.io Setup ─────────────────────────────────────────
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:3000',
-      'http://127.0.0.1:3000',
-      process.env.CLIENT_URL || '*'],
+    origin: "https://openworkfyp.netlify.app" || "http://localhost:3000",
     methods: ['GET', 'POST'],
     credentials: true,
   },
@@ -81,7 +79,7 @@ if (process.env.NODE_ENV === 'production') {
 
 // ─── CORS ────────────────────────────────────────────────────
 app.use(cors({
-  origin: "https://openworkfyp.netlify.app",
+  origin: "https://openworkfyp.netlify.app" || "http://localhost:3000",
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
